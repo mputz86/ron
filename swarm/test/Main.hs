04de46132ad4c6afdbd19f6ab9591f6f6ddc57c4
Yuriy Syrovetskiy <cblp@cblp.su>
2019-04-12 10:29:25 +0300

    WIP

--- swarm/test/Main.hs
+++ swarm/test/Main.hs
@@ -8,22 +8,31 @@
 module Main (main) where
 
 import           Control.Monad.IO.Class (liftIO)
-import           Hedgehog (failure, forAll, property, success, (===))
+import           GHC.Stack (HasCallStack, withFrozenCallStack)
+import           Hedgehog (MonadTest, forAll, property, (===))
+import           Hedgehog.Internal.Property (failWith)
 import           Test.Tasty.Hedgehog (testProperty)
 import           Test.Tasty.TH (defaultMainGenerator)
 
 import qualified Gen
 
-import           Swarm.DB.Replica (get, newTextReplica)
-import           Swarm.RON.Status (Status (Status), code, notOpen)
+import           Swarm.DB.Replica (open, get, newTextReplica)
+import           Swarm.RON.Status (Status (Status), notOpen, ok)
 
 main = $defaultMainGenerator
 
 prop_uninitialized_replica = property $ do
     replica <- liftIO newTextReplica
-    key <- forAll Gen.uuid
-    liftIO (get key replica) >>= \case
-        Left status@Status{code}
-            | code == notOpen -> success
-            | otherwise       -> status === Status notOpen ""
-        Right _ -> failure
+    objectId <- forAll Gen.uuid
+    liftIO (get objectId replica) >>= expectLeft >>= (=== Status notOpen "")
+
+prop_put_get = property $ do
+    objectId <- forAll Gen.uuid
+    replica <- liftIO newTextReplica
+    liftIO (open "/tmp/swarm.3" replica) >>= (=== Status ok "")
+    liftIO (get objectId replica) >>= expectLeft >>= (=== Status notOpen "")
+
+expectLeft :: (MonadTest m, HasCallStack) => Either a x -> m a
+expectLeft = \case
+    Left x  -> pure x
+    Right _ -> withFrozenCallStack $ failWith Nothing "unexpected Right"
